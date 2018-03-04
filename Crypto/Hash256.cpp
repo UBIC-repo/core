4b47bcdb35ef08bf96edd0d096b602adcdde03b8
ubicorn <ubicorn@protonmail.com>
2018-03-04 00:11:30 +0000

    Code clean up

--- Crypto/Hash256.cpp
+++ Crypto/Hash256.cpp
@@ -3,14 +3,8 @@
 #include "Hash256.h"
 #include "Sha256.h"
 
-void Hash256::hash256(unsigned char *message, unsigned int messageLength, unsigned char *digest) {
-    Sha256::sha256(message, messageLength, digest);
-    Sha256::sha256(digest, 32, digest);
-}
-
 std::vector<unsigned char> Hash256::hash256(std::vector<unsigned char> message) {
     std::vector<unsigned char> sha256 = Sha256::sha256(message);
-    sha256 = Sha256::sha256(message);
 
     return sha256;
 }
