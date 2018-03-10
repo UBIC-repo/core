dfe76372a8f8079fbedb4d5b398b178675aa7e50
ubicorn <ubicorn@protonmail.com>
2018-03-10 11:54:54 +0000

    Added CLI functionalities

--- cli/HttpClient.h
+++ cli/HttpClient.h
@@ -6,8 +6,9 @@
 
 class HttpClient {
 public:
+    static std::string url_encode(const std::string &value);
     std::string get(std::string url, std::string apiKey);
-    //std::string post();
+    std::string post(std::string url, std::string apiKey, std::string post);
 };
 
 
