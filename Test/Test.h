862c5bbf67cfaa3bef40173edba25e11cb48e510
ubicorn <ubicorn@protonmail.com>
2018-05-10 13:55:34 +0000

    Optimize Certificate import

--- Test/Test.h
+++ Test/Test.h
@@ -9,8 +9,8 @@ class Test {
 public:
     static uint8_t getCurrencyIdFromIso2Code(char* iso2code);
     static time_t ASN1_GetTimeT(ASN1_TIME* time);
-    static void importCACerts(BlockHeader* header);
-    static void importDSCCerts(BlockHeader* header);
+    static void importCACerts();
+    static void importDSCCerts();
 };
 
 
