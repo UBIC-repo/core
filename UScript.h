d1a111852eee587ea8fc3b17533a2d6700e4ba01
ubicorn <ubicorn@protonmail.com>
2018-05-21 11:21:49 +0000

    New Script NtpskAlreadyUsedScript

--- UScript.h
+++ UScript.h
@@ -147,4 +147,34 @@ struct PkhInScript {
     }
 };
 
+struct NtpskAlreadyUsedScript {
+    std::vector<unsigned char> address;
+    std::vector<unsigned char> dscID;
+
+    const std::vector<unsigned char> &getAddress() const {
+        return address;
+    }
+
+    void setAddress(const std::vector<unsigned char> &address) {
+        NtpskAlreadyUsedScript::address = address;
+    }
+
+    const std::vector<unsigned char> &getDscID() const {
+        return dscID;
+    }
+
+    void setDscID(const std::vector<unsigned char> &dscID) {
+        NtpskAlreadyUsedScript::dscID = dscID;
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(address);
+        READWRITE(dscID);
+    }
+
+};
+
 #endif //TX_USCRIPT_H
