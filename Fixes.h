69615a2e5adbe3e7506607627b440afe41ffbcc0
ubicorn <ubicorn@protonmail.com>
2018-07-21 19:09:50 +0000

    Implement wrong currencyID fix

---
+++ Fixes.h
@@ -0,0 +1,19 @@
+/**
+ * Mistakes can happen, this class intends to fix them
+ */
+
+#ifndef UBICD_FIXES_H
+#define UBICD_FIXES_H
+
+
+#include <stdint-gcc.h>
+#include <vector>
+
+class Fixes {
+public:
+    // fixes certificates with wrong currency ID
+    static uint8_t fixCertificateCurrencyID(std::vector<unsigned char> certificateID, uint8_t currencyID);
+};
+
+
+#endif //UBICD_FIXES_H
