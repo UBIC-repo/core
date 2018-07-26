9e1985491d45db92624ef3914c16518017114e4a
ubicorn <ubicorn@protonmail.com>
2018-07-26 00:26:02 +0000

    Fix memory leaks

--- Network/BlockCache.h
+++ Network/BlockCache.h
@@ -64,6 +64,7 @@ private:
                         cacheMutex.unlock();
                     }
                 } else {
+                    free(blockHeader);
                     blockIt++;
                 }
             }
