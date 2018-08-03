d0b9e69621f90ed56f12d4a71d2209cb7c456d60
ubicorn <ubicorn@protonmail.com>
2018-08-03 22:37:41 +0000

    Fix: When multiple passports are linked to one address

--- Address.h
+++ Address.h
@@ -24,11 +24,40 @@ public:
     }
 };
 
-
-class AddressForStore : public Address {
+class DscToAddressLink {
 private:
     std::vector<unsigned char> dscCertificate;
     uint32_t DSCLinkedAtHeight = 0;
+
+public:
+    const std::vector<unsigned char> &getDscCertificate() const {
+        return dscCertificate;
+    }
+
+    void setDscCertificate(const std::vector<unsigned char> &dscCertificate) {
+        DscToAddressLink::dscCertificate = dscCertificate;
+    }
+
+    uint32_t getDSCLinkedAtHeight() const {
+        return DSCLinkedAtHeight;
+    }
+
+    void setDSCLinkedAtHeight(uint32_t DSCLinkedAtHeight) {
+        DscToAddressLink::DSCLinkedAtHeight = DSCLinkedAtHeight;
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(dscCertificate);
+        READWRITE(DSCLinkedAtHeight);
+    }
+};
+
+class AddressForStore : public Address {
+private:
+    std::vector<DscToAddressLink> dscToAddressLinks;
     UAmount UBIdebit;
     UAmount amount;
     uint32_t nonce = 0;
@@ -50,22 +79,6 @@ public:
         AddressForStore::nonce = nonce;
     }
 
-    std::vector<unsigned char> getDscCertificate() {
-        return dscCertificate;
-    }
-
-    void setDscCertificate(std::vector<unsigned char> dscCertificate) {
-        AddressForStore::dscCertificate = dscCertificate;
-    }
-
-    uint32_t getDSCLinkedAtHeight() {
-        return DSCLinkedAtHeight;
-    }
-
-    void setDSCLinkedAtHeight(uint32_t DSCLinkedAtHeight) {
-        AddressForStore::DSCLinkedAtHeight = DSCLinkedAtHeight;
-    }
-
     UAmount getUBIdebit() {
         return UBIdebit;
     }
@@ -74,14 +87,21 @@ public:
         AddressForStore::UBIdebit = UBIdebit;
     }
 
+    std::vector<DscToAddressLink> getDscToAddressLinks() const {
+        return dscToAddressLinks;
+    }
+
+    void setDscToAddressLinks(const std::vector<DscToAddressLink> dscToAddressLinks) {
+        AddressForStore::dscToAddressLinks = dscToAddressLinks;
+    }
+
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action) {
         READWRITE(nonce);
         READWRITE(script);
-        READWRITE(dscCertificate);
-        READWRITE(DSCLinkedAtHeight);
+        READWRITE(dscToAddressLinks);
         READWRITE(UBIdebit);
         READWRITE(amount);
     }
