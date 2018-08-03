0fceeb34946596172334cb72ea929057f19695e9
ubicorn <ubicorn@protonmail.com>
2018-08-03 22:39:43 +0000

    Removed unused code

--- UBICalculator.h
+++ UBICalculator.h
@@ -10,10 +10,7 @@
 
 class UBICalculator {
 public:
-    static bool isAddressConnectedToADSC(std::vector<unsigned char> address);
     static bool isAddressConnectedToADSC(AddressForStore* address);
-    static UAmount totalReceivedUBI(std::vector<unsigned char> address);
-    static UAmount totalReceivedUBI(std::vector<unsigned char> address, uint32_t blockHeight);
     static UAmount totalReceivedUBI(AddressForStore* addressForStore);
     static UAmount totalReceivedUBI(AddressForStore* addressForStore, uint32_t blockHeight);
 };
