a7c87165deae761fcd2132f1d3e0ba44e8e72cc8
ubicorn <ubicorn@protonmail.com>
2018-09-28 18:49:53 +0000

    finish MRZParser

--- KYC/MRZParser.h
+++ KYC/MRZParser.h
@@ -8,6 +8,8 @@
 
 class MRZParser {
 private:
+    std::string rtrim(std::string s);
+    std::string formatClean(std::string str);
     MRZResponseObject parseTD1(std::vector<unsigned char> mrz);
     MRZResponseObject parseTD2(std::vector<unsigned char> mrz);
     MRZResponseObject parseTD3(std::vector<unsigned char> mrz);
