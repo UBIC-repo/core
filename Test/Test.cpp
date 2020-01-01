0371e7c4f724633dedec424acf9143a0695b7eb4
ubicorn <ubicorn@protonmail.com>
2020-01-01 06:25:53 +0000

    Code simplification

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -10,174 +10,7 @@
 #include "../Wallet.h"
 #include "../Time.h"
 #include "../Scripts/AddCertificateScript.h"
-
-uint8_t Test::getCurrencyIdFromIso2Code(char* iso2code) {
-    uint8_t currencyId = 0;
-    if(strcmp(iso2code, "AT") == 0 || strcmp(iso2code, "at") == 0) {
-        currencyId = CURRENCY_AUSTRIA;
-    }
-
-    if(strcmp(iso2code, "DE") == 0 || strcmp(iso2code, "de") == 0) {
-        currencyId = CURRENCY_GERMANY;
-    }
-
-    if(strcmp(iso2code, "FR") == 0 || strcmp(iso2code, "fr") == 0
-       || strcmp(iso2code, "Certificat HSM Document Signer G350233") == 0
-       || strcmp(iso2code, "HSM Document Signer G350259") == 0
-       || strcmp(iso2code, "Certificat HSM Document Signer H480083") == 0
-    ) {
-        currencyId = CURRENCY_FRANCE;
-    }
-
-    if(strcmp(iso2code, "SE") == 0 || strcmp(iso2code, "se") == 0
-       || strcmp(iso2code, "Swedish National ID Card Document Signer") == 0
-       || strcmp(iso2code, "Swedish Country Signing CA v2") == 0
-       || strcmp(iso2code, "Swedish Country Signing CA") == 0
-       || strcmp(iso2code, "Swedish Passport Document Signer") == 0
-       || strcmp(iso2code, "Swedish Residence Card Document Signer") == 0
-       || strcmp(iso2code, "Swedish Residence Permit Document Signer") == 0
-       || strcmp(iso2code, "Swedish Emergency Passport Document Signer") == 0
-       || strcmp(iso2code, "Swedish RP Document Signer") == 0
-            ) {
-        currencyId = CURRENCY_SWEDEN;
-    }
-
-    if(strcmp(iso2code, "CA") == 0 || strcmp(iso2code, "ca") == 0) {
-        currencyId = CURRENCY_CANADA;
-    }
-
-    if(strcmp(iso2code, "IE") == 0 || strcmp(iso2code, "ie") == 0) {
-        currencyId = CURRENCY_IRELAND;
-    }
-
-    if(strcmp(iso2code, "CN") == 0 || strcmp(iso2code, "cn") == 0) {
-        currencyId = CURRENCY_CHINA;
-    }
-
-    if(strcmp(iso2code, "GB") == 0 || strcmp(iso2code, "gb") == 0) {
-        currencyId = CURRENCY_UNITED_KINGDOM;
-    }
-
-    if(strcmp(iso2code, "AE") == 0 || strcmp(iso2code, "ae") == 0) {
-        currencyId = CURRENCY_UNITED_ARAB_EMIRATES;
-    }
-
-    if(strcmp(iso2code, "NZ") == 0 || strcmp(iso2code, "nz") == 0) {
-        currencyId = CURRENCY_NEW_ZEALAND;
-    }
-
-    if(strcmp(iso2code, "FI") == 0 || strcmp(iso2code, "fi") == 0) {
-        currencyId = CURRENCY_FINLAND;
-    }
-
-    if(strcmp(iso2code, "LU") == 0 || strcmp(iso2code, "lu") == 0
-       || strcmp(iso2code, "Grand-Duchy of Luxembourg Country Signing CA") == 0
-       || strcmp(iso2code, "Grand-Duchy of Luxembourg CSCA eTravel Documents") == 0) {
-        currencyId = CURRENCY_LUXEMBOURG;
-    }
-
-    if(strcmp(iso2code, "SG") == 0 || strcmp(iso2code, "sg") == 0) {
-        currencyId = CURRENCY_SINGAPORE;
-    }
-
-    if(strcmp(iso2code, "HU") == 0 || strcmp(iso2code, "hu") == 0) {
-        currencyId = CURRENCY_HUNGARY;
-    }
-
-    if(strcmp(iso2code, "CZ") == 0 || strcmp(iso2code, "cz") == 0
-       || strcmp(iso2code, "STC_DS_1") == 0) {
-        currencyId = CURRENCY_CZECH_REPUBLIC;
-    }
-
-    if(strcmp(iso2code, "MY") == 0 || strcmp(iso2code, "my") == 0) {
-        currencyId = CURRENCY_MALAYSIA;
-    }
-
-    if(strcmp(iso2code, "UA") == 0 || strcmp(iso2code, "ua") == 0) {
-        currencyId = CURRENCY_UKRAINE;
-    }
-
-    if(strcmp(iso2code, "EE") == 0 || strcmp(iso2code, "ee") == 0) {
-        currencyId = CURRENCY_ESTONIA;
-    }
-
-    if(strcmp(iso2code, "MC") == 0 || strcmp(iso2code, "mc") == 0) {
-        currencyId = CURRENCY_MONACO;
-    }
-
-    if(strcmp(iso2code, "LI") == 0 || strcmp(iso2code, "li") == 0) {
-        currencyId = CURRENCY_LIECHTENSTEIN;
-    }
-    
-    if(strcmp(iso2code, "IS") == 0 || strcmp(iso2code, "is") == 0) {
-        currencyId = CURRENCY_ICELAND;
-    }
-
-    if(strcmp(iso2code, "HK") == 0 || strcmp(iso2code, "hk") == 0) {
-        currencyId = CURRENCY_HONG_KONG;
-    }
-
-    if(strcmp(iso2code, "ES") == 0 || strcmp(iso2code, "es") == 0) {
-        currencyId = CURRENCY_SPAIN;
-    }
-    
-    if(strcmp(iso2code, "US") == 0 || strcmp(iso2code, "us") == 0) {
-        currencyId = CURRENCY_USA;
-    }
-
-    if(strcmp(iso2code, "AU") == 0 || strcmp(iso2code, "au") == 0) {
-        currencyId = CURRENCY_AUSTRALIA;
-    }
-
-    if(strcmp(iso2code, "CH") == 0) {
-        currencyId = CURRENCY_SWITZERLAND;
-    }
-
-    if(strcmp(iso2code, "JP") == 0) {
-        currencyId = CURRENCY_JAPAN;
-    }
-
-    if(strcmp(iso2code, "TH") == 0) {
-        currencyId = CURRENCY_THAILAND;
-    }
-
-    return currencyId;
-}
-
-time_t Test::ASN1_GetTimeT(ASN1_TIME* time)
-{
-    struct tm t;
-    const char* str = (const char*) time->data;
-    size_t i = 0;
-
-    memset(&t, 0, sizeof(t));
-
-    if (time->type == V_ASN1_UTCTIME) {/* two digit year */
-        t.tm_year = (str[i++] - '0') * 10;
-        t.tm_year += (str[i++] - '0');
-        if (t.tm_year < 70)
-            t.tm_year += 100;
-    } else if (time->type == V_ASN1_GENERALIZEDTIME) {/* four digit year */
-        t.tm_year = (str[i++] - '0') * 1000;
-        t.tm_year+= (str[i++] - '0') * 100;
-        t.tm_year+= (str[i++] - '0') * 10;
-        t.tm_year+= (str[i++] - '0');
-        t.tm_year -= 1900;
-    }
-    t.tm_mon  = (str[i++] - '0') * 10;
-    t.tm_mon += (str[i++] - '0') - 1; // -1 since January is 0 not 1.
-    t.tm_mday = (str[i++] - '0') * 10;
-    t.tm_mday+= (str[i++] - '0');
-    t.tm_hour = (str[i++] - '0') * 10;
-    t.tm_hour+= (str[i++] - '0');
-    t.tm_min  = (str[i++] - '0') * 10;
-    t.tm_min += (str[i++] - '0');
-    t.tm_sec  = (str[i++] - '0') * 10;
-    t.tm_sec += (str[i++] - '0');
-
-    /* Note: we did not adjust the time based on time zone information */
-    return mktime(&t);
-}
+#include "../CertStore/CertHelper.h"
 
 void Test::importCACerts() {
 
@@ -228,6 +61,9 @@ void Test::importCACerts() {
 
     for(std::vector<unsigned char> file: fileList) {
         Log(LOG_LEVEL_INFO) << "DIR: " <<  file.data();
+        if (!FS::isDir(file)) {
+            continue;
+        }
         std::vector<std::vector<unsigned char> > fileList2 = FS::readDir(file);
 
         for(std::vector<unsigned char> file2: fileList2) {
@@ -249,13 +85,13 @@ void Test::importCACerts() {
             const unsigned char *countryStr = ASN1_STRING_get0_data(countryData);
             Log(LOG_LEVEL_INFO) << "Country code: " << countryStr;
 
-            if(getCurrencyIdFromIso2Code((char*)countryStr) == 0) {
+            if(CertHelper::getCurrencyIdForCert(x509) == 0) {
                 Log(LOG_LEVEL_INFO) << "Could not get currency ID for countryStr:" << countryStr;
                 return;
                 continue;
             }
 
-            time_t notAfterTime = Test::ASN1_GetTimeT(notAfter);
+            time_t notAfterTime = CertHelper::ASN1_GetTimeT(notAfter);
 
             uint64_t notAfter64 = *reinterpret_cast<uint64_t*>(&notAfterTime);
 
@@ -265,7 +101,7 @@ void Test::importCACerts() {
             }
 
             ca->setExpirationDate(notAfter64);
-            ca->setCurrencyId(Test::getCurrencyIdFromIso2Code((char*)countryStr));
+            ca->setCurrencyId(CertHelper::getCurrencyIdForCert(x509));
 
             std::vector<unsigned char> id = ca->getId();
             Log(LOG_LEVEL_INFO) << "CSCA id: " << id;
@@ -319,7 +155,7 @@ void Test::importCACerts() {
             TransactionForNetwork transactionForNetwork;
             transactionForNetwork.setTransaction(*tx);
 
-            txPool.appendTransaction(transactionForNetwork);
+            //txPool.appendTransaction(transactionForNetwork);
             cscaCounter++;
             //certStore.addCSCA(ca, header);
         }
@@ -346,6 +182,10 @@ void Test::importDSCCerts() {
     uint32_t dscCounter = 0;
 
     for(std::vector<unsigned char> file: fileList) {
+        Log(LOG_LEVEL_INFO) << "DIR: " <<  file.data();
+        if (!FS::isDir(file)) {
+            //continue;
+        }
         std::vector<std::vector<unsigned char> > fileList2 = FS::readDir(file);
 
         for(std::vector<unsigned char> file2: fileList2) {
@@ -357,81 +197,16 @@ void Test::importDSCCerts() {
             Log(LOG_LEVEL_INFO) << "DSC path:" << pData;
 
             X509* x509 = dsc->getX509();
-            ASN1_TIME* notAfter = X509_getm_notAfter(x509);
-            ASN1_TIME* notBefore = X509_getm_notBefore(x509);
-            X509_NAME *subject = X509_get_subject_name(x509);
 
-            X509_NAME_ENTRY *country = X509_NAME_get_entry(subject, 0);
-            ASN1_STRING *countryData = X509_NAME_ENTRY_get_data(country);
-            const unsigned char *countryStr = ASN1_STRING_get0_data(countryData);
+            uint8_t currencyId = CertHelper::getCurrencyIdForCert(x509);
 
-            Log(LOG_LEVEL_INFO) << "Country code: " << countryStr;
-
-            if(getCurrencyIdFromIso2Code((char*)countryStr) == 0) {
-                Log(LOG_LEVEL_INFO) << "Could not get currency ID for countryStr:" << countryStr;
+            if(currencyId == 0) {
+                Log(LOG_LEVEL_INFO) << "Could not get currency ID for countryStr:" << currencyId;
                 return;
-            }
-
-            Log(LOG_LEVEL_INFO) << "notAfter: " << notAfter->data;
-            Log(LOG_LEVEL_INFO) << "notBefore: " << notBefore->data;
-
-            time_t notAfterTime = Test::ASN1_GetTimeT(notAfter);
-            time_t notBeforeTime = Test::ASN1_GetTimeT(notBefore);
-
-            uint64_t notAfter64 = *reinterpret_cast<uint64_t*>(&notAfterTime);
-            uint64_t notBefore64 = *reinterpret_cast<uint64_t*>(&notBeforeTime);
-
-            Log(LOG_LEVEL_INFO) << "notAfter: " << notAfter64;
-            Log(LOG_LEVEL_INFO) << "notBefore: " << notBefore64;
-
-            uint32_t maxValidity = 0;
-            uint8_t currencyId = Test::getCurrencyIdFromIso2Code((char*)countryStr);
-            uint32_t tenYears = 10 * 365 * 24 * 3600;
-            uint32_t fiveYears = 5 * 365 * 24 * 3600;
-
-            if(strcmp((char*)countryStr, "AT") == 0 || strcmp((char*)countryStr, "at") == 0 ||
-               strcmp((char*)countryStr, "DE") == 0 || strcmp((char*)countryStr, "de") == 0 ||
-               strcmp((char*)countryStr, "CN") == 0 || strcmp((char*)countryStr, "cn") == 0 ||
-               strcmp((char*)countryStr, "GB") == 0 || strcmp((char*)countryStr, "gb") == 0 ||
-               strcmp((char*)countryStr, "AU") == 0 || strcmp((char*)countryStr, "au") == 0 ||
-               strcmp((char*)countryStr, "IE") == 0 || strcmp((char*)countryStr, "ie") == 0 ||
-               strcmp((char*)countryStr, "NZ") == 0 || strcmp((char*)countryStr, "nz") == 0 ||
-               strcmp((char*)countryStr, "CZ") == 0 || strcmp((char*)countryStr, "cz") == 0 ||
-               strcmp((char*)countryStr, "CA") == 0 || strcmp((char*)countryStr, "ca") == 0 ||
-               strcmp((char*)countryStr, "UA") == 0 || strcmp((char*)countryStr, "ua") == 0 ||
-               strcmp((char*)countryStr, "US") == 0 || strcmp((char*)countryStr, "us") == 0 ||
-               strcmp((char*)countryStr, "JP") == 0 || strcmp((char*)countryStr, "jp") == 0 ||
-               strcmp((char*)countryStr, "HU") == 0 || strcmp((char*)countryStr, "hu") == 0 ||
-               strcmp((char*)countryStr, "LI") == 0 || strcmp((char*)countryStr, "li") == 0 ||
-               strcmp((char*)countryStr, "CH") == 0 || strcmp((char*)countryStr, "ch") == 0 ||
-               strcmp((char*)countryStr, "IS") == 0 || strcmp((char*)countryStr, "is") == 0 ||
-               strcmp((char*)countryStr, "FR") == 0 || strcmp((char*)countryStr, "fr") == 0) {
-                maxValidity = tenYears;
-            }
-
-            if(strcmp((char*)countryStr, "SE") == 0 || strcmp((char*)countryStr, "se") == 0 ||
-               strcmp((char*)countryStr, "FI") == 0 || strcmp((char*)countryStr, "fi") == 0 ||
-               strcmp((char*)countryStr, "MY") == 0 || strcmp((char*)countryStr, "my") == 0 ||
-               strcmp((char*)countryStr, "TH") == 0 || strcmp((char*)countryStr, "th") == 0 ||
-               strcmp((char*)countryStr, "SG") == 0 || strcmp((char*)countryStr, "sg") == 0 ||
-               strcmp((char*)countryStr, "MC") == 0 || strcmp((char*)countryStr, "mc") == 0 ||
-               strcmp((char*)countryStr, "EE") == 0 || strcmp((char*)countryStr, "ee") == 0 ||
-               strcmp((char*)countryStr, "LU") == 0 || strcmp((char*)countryStr, "lu") == 0 ||
-               strcmp((char*)countryStr, "AE") == 0 || strcmp((char*)countryStr, "ae") == 0
-               ) {
-                maxValidity = fiveYears;
-            }
-
-            if(maxValidity == 0) {
-                Log(LOG_LEVEL_ERROR) << "maxValidity == 0";
                 continue;
             }
 
-            uint64_t expiration = notAfter64;
-            if(notBefore64 + maxValidity < expiration) {
-                expiration = notBefore64 + maxValidity;
-            }
-
+            uint64_t expiration = CertHelper::calculateDSCExpirationDateForCert(x509);
             /*
             if(expiration < 1640995200) {
                 expiration = Time::getCurrentTimestamp() + 540;
@@ -502,10 +277,10 @@ void Test::importDSCCerts() {
 
             TransactionForNetwork transactionForNetwork;
             transactionForNetwork.setTransaction(*tx);
-
+            /*
             if(txPool.appendTransaction(transactionForNetwork)) {
                 Log(LOG_LEVEL_INFO) << "appended ADD Certificate to transactions";
-            }
+            }*/
             dscCounter++;
         }
     }
