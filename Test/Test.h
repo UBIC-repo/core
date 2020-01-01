324c72220c7a72ad852e5c11b1660a2a05208c3c
ubicorn <ubicorn@protonmail.com>
2020-01-01 06:25:23 +0000

    Code simplification

--- Test/Test.h
+++ Test/Test.h
@@ -7,8 +7,6 @@
 
 class Test {
 public:
-    static uint8_t getCurrencyIdFromIso2Code(char* iso2code);
-    static time_t ASN1_GetTimeT(ASN1_TIME* time);
     static void importCACerts();
     static void importDSCCerts();
 };
