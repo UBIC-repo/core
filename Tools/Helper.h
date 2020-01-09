6791e36c29e693bb731e80ea4974b7b77b7336ed
ubicorn <ubicorn@protonmail.com>
2020-01-09 11:35:27 +0000

    Added Helper.h

---
+++ Tools/Helper.h
@@ -0,0 +1,26 @@
+
+
+#ifndef UBICD_HELPER_H
+#define UBICD_HELPER_H
+
+
+#include <bio.h>
+#include <string>
+#include <err.h>
+
+class Helper {
+public:
+    static std::string getOpenSSLError()
+    {
+        BIO *bio = BIO_new(BIO_s_mem());
+        ERR_print_errors(bio);
+        char *buf;
+        size_t len = BIO_get_mem_data(bio, &buf);
+        std::string ret(buf, len);
+        BIO_free(bio);
+        return ret;
+    }
+};
+
+
+#endif //UBICD_HELPER_H
