fb8af2e48c5d2706fc28938be555e8f784f77999
ubicorn <ubicorn@protonmail.com>
2020-04-09 12:21:56 +0000

    Better testing

--- Test/Test.h
+++ Test/Test.h
@@ -9,6 +9,10 @@ class Test {
 public:
     static void importCACerts();
     static void importDSCCerts();
+    static void createRootCert();
+    static void createValidators();
+    static X509* create509(const unsigned char* c, const unsigned char* cn1, const unsigned char* cn2, X509* signer, EVP_PKEY *signerPkey, std::vector<unsigned char> privateKeyVector);
+    static void sanitizeUbicFolder();
 };
 
 
