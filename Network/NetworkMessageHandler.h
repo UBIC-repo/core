222b9315ec6de0c2f4fd4abf0c4af05ff1f2d7b3
ubicorn <ubicorn@protonmail.com>
2020-04-17 18:43:52 +0000

    Code refactoring

--- Network/NetworkMessageHandler.h
+++ Network/NetworkMessageHandler.h
@@ -20,16 +20,16 @@ private:
     static void handleAskForStatus(PeerInterfacePtr recipient);
     static void handleAskForDonationAddress(PeerInterfacePtr recipient);
     static void handleTransmitBlock(std::vector<unsigned char> block, PeerInterfacePtr recipient);
-    static void handleTransmitTransactions(TransmitTransactions *transmitBlocks, PeerInterfacePtr recipient);
-    static void handleTransmitBlock(TransmitBlock *transmitBlock, PeerInterfacePtr recipient);
-    static void handleTransmitBlocks(TransmitBlocks *transmitBlocks, PeerInterfacePtr recipient);
-    static void handleTransmitPeers(TransmitPeers *transmitPeers, PeerInterfacePtr recipient);
-    static void handleTransmitBlockchainHeight(TransmitBlockchainHeight *transmitBlockchainHeight, PeerInterfacePtr recipient);
-    static void handleTransmitBestBlockHeader(TransmitBestBlockHeader *transmitBestBlockHeader, PeerInterfacePtr recipient);
-    static void handleTransmitVersion(TransmitVersion *transmitVersion, PeerInterfacePtr recipient);
-    static void handleTransmitStatus(TransmitStatus *transmitStatus, PeerInterfacePtr recipient);
+    static void handleTransmitTransactions(TransmitTransactions transmitTransactions, PeerInterfacePtr recipient);
+    static void handleTransmitBlock(TransmitBlock transmitBlock, PeerInterfacePtr recipient);
+    static void handleTransmitBlocks(TransmitBlocks transmitBlocks, PeerInterfacePtr recipient);
+    static void handleTransmitPeers(TransmitPeers transmitPeers, PeerInterfacePtr recipient);
+    static void handleTransmitBlockchainHeight(TransmitBlockchainHeight transmitBlockchainHeight, PeerInterfacePtr recipient);
+    static void handleTransmitBestBlockHeader(TransmitBestBlockHeader transmitBestBlockHeader, PeerInterfacePtr recipient);
+    static void handleTransmitVersion(TransmitVersion transmitVersion, PeerInterfacePtr recipient);
+    static void handleTransmitStatus(TransmitStatus transmitStatus, PeerInterfacePtr recipient);
     static void handleTransmitLeave(PeerInterfacePtr recipient);
-    static void handleTransmitDonationAddress(TransmitDonationAddress* transmitDonationAddress, PeerInterfacePtr recipient);
+    static void handleTransmitDonationAddress(TransmitDonationAddress transmitDonationAddress, PeerInterfacePtr recipient);
 public:
     static void handleNetworkMessage(NetworkMessage *networkMessage, PeerInterfacePtr recipient);
 };
