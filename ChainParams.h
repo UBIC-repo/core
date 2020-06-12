0e2c81805620910728f4d9abec3fc2ecbe8fb6b9
ubicorn <ubicorn@protonmail.com>
2020-06-12 19:30:23 +0000

    New version 0.6.1

--- ChainParams.h
+++ ChainParams.h
@@ -8,8 +8,8 @@
 #include <openssl/ossl_typ.h>
 #include <openssl/ec.h>
 
-#define VERSION "0.6.0"
-#define VERSION_16BITS 60
+#define VERSION "0.6.1"
+#define VERSION_16BITS 61
 
 #define CURRENCY_SWITZERLAND 1
 #define CURRENCY_GERMANY 2
