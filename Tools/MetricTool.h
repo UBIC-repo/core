9e7bf4dc48cd25f9817aa88c881ef7766b1b8189
ubicorn <ubicorn@protonmail.com>
2020-06-28 03:34:01 +0000

    Fix metrics

--- Tools/MetricTool.h
+++ Tools/MetricTool.h
@@ -7,11 +7,13 @@
 
 class MetricTool {
 private:
+    static uint64_t findFirstPassportBlockHeight(uint8_t currencyId);
     static uint64_t calculateTotalSupply(
             uint64_t emissionRate,
             uint64_t developmentEmission,
             uint64_t delegateEmission,
-            uint32_t startingBlockHeight
+            uint32_t startingBlockHeight,
+            uint8_t currencyId
     );
 public:
     static uint64_t getPassportBlockSupply(uint8_t currencyId);
